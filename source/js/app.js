4ba824f1bad47d7b652e556b319c3353f5fb5561
Trent Oswald <trentoswald@therebelrobot.com>
2014-05-13 15:04:17 -0600

    polishing for immediate launch

--- source/js/app.js
+++ source/js/app.js
@@ -10,6 +10,27 @@ var model = {
 		  	openModal: function(which){
 		  		model.vue.modalOpen = true;
 		  		model.vue.modalType = which;
+		  	},
+		  	openWindow: function(which){
+		  		var url;
+		  		switch (which){
+		  			case 'ngless':
+		  				url = "http://github.com/therebelrobot/generator-ngless";
+		  				break;
+		  			case 'maraca':
+		  				url = "http://therebelrobot.github.com/maraca";
+		  				break;
+		  			case 'octostar':
+		  				url = "http://github.com/therebelrobot/octostar";
+		  				break;
+		  			case 'peel':
+		  				url = "http://github.com/therebelrobot/peel";
+		  				break;
+		  			case 'trr':
+		  				url = "http://github.com/therebelrobot/trr-mar14";
+		  				break;
+		  		}
+		  		window.open(url);
 		  	}
 		  }
 		})
