2ab6e065e877a5f119d1efedb064b6d0225182f0
Trent Oswald <trentoswald@therebelrobot.com>
2014-08-08 09:40:14 -0600

    updated portfolio

--- source/js/app.js
+++ source/js/app.js
@@ -1,42 +1,45 @@
 var model = {
-	initvue: function(){
+	initvue: function() {
 		model.vue = new Vue({
-		  el: '#v-app',
-		  data: {
-		    modalOpen:false,
-		    modalType:false
-		  },
-		  methods:{
-		  	openModal: function(which){
-		  		model.vue.modalOpen = true;
-		  		model.vue.modalType = which;
-		  	},
-		  	openWindow: function(which){
-		  		var url;
-		  		switch (which){
-		  			case 'ngless':
-		  				url = "http://github.com/therebelrobot/generator-ngless";
-		  				break;
-		  			case 'maraca':
-		  				url = "http://therebelrobot.github.com/maraca";
-		  				break;
-		  			case 'octostar':
-		  				url = "http://github.com/therebelrobot/octostar";
-		  				break;
-		  			case 'peel':
-		  				url = "http://github.com/therebelrobot/peel";
-		  				break;
-		  			case 'trr':
-		  				url = "http://github.com/therebelrobot/trr-mar14";
-		  				break;
-		  		}
-		  		window.open(url);
-		  	}
-		  }
+			el: '#v-app',
+			data: {
+				modalOpen: false,
+				modalType: false
+			},
+			methods: {
+				openModal: function(which) {
+					model.vue.modalOpen = true;
+					model.vue.modalType = which;
+				},
+				openWindow: function(which) {
+					var url;
+					switch (which) {
+						case 'grow':
+							url = "http://grow.com";
+							break;
+						case 'maraca':
+							url = "http://therebelrobot.github.com/maraca";
+							break;
+						case 'constella':
+							url = "http://github.com/therebelrobot/octostar";
+							break;
+						case 'moki':
+							url = "http://moki.com/";
+							break;
+						case 'trr':
+							url = "http://github.com/therebelrobot/trr-mar14";
+							break;
+						case 'pnn':
+							url = "http://provo-nodejs-ninjas.github.io/";
+							break;
+					}
+					window.open(url);
+				}
+			}
 		})
 	}
 }
 
-$(document).ready(function(){
+$(document).ready(function() {
 	model.initvue();
 });
